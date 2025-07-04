1d361f33b1457c1a17dac5de2e9a890a11826251
Mateus Dantas <mtzdantas@gmail.com>
2025-07-04 10:46:50 -0300

    feat: adicionando toasts

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import { ToastProvider } from './hooks/useToast';
+import ToastContainer from './components/ToastContainer';
 import './index.css'
 import Sidebar from './components/Sidebar';
 import Home from "./pages/Home";
@@ -7,16 +9,19 @@ import Import from "./pages/Import";
 
 function App() {
   return (
-    <div className="flex">
-      <Sidebar />
-      <main className="flex-1">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/import" element={<Import />} />
-        </Routes>
-      </main>
-    </div>
+    <ToastProvider>
+      <div className="flex">
+        <Sidebar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/import" element={<Import />} />
+          </Routes>
+        </main>
+      </div>
+      <ToastContainer />
+    </ToastProvider>
   )
 }
 
